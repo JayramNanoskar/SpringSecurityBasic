56c8e0115888cafe41d4fbf8cec14ed7cc50eaca
JayramNanoskar <jayram.n978@gmail.com>
2021-11-29 16:03:44 +0530

    Setting LDAP authentication with Spring Security

---
+++ spring-security-ldap/src/main/java/com/jayram/springsecurityldap/controller/HomeResource.java
@@ -0,0 +1,13 @@
+package com.jayram.springsecurityldap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String index() {
+		return "Welcome Page";
+	}
+}
